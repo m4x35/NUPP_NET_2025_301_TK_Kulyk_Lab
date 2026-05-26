6c8f147a33558d6f336ec7e194010b50b17cd9ac
m4x35 <159756178+m4x35@users.noreply.github.com>
2026-05-26 16:13:49 +0300

    Completed lab5

---
+++ бібліотекаЛАБ5/SimpleLibrary.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleLibrary.Infrastructure;
+using SimpleLibrary.Infrastructure.Models;
+using SimpleLibrary.Infrastructure.Repositories;
+using SimpleLibrary.Infrastructure.Services;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var options = new DbContextOptionsBuilder<SimpleLibraryContext>()
+    .UseSqlite("Data Source=simple_library.db")
+    .Options;
+
+using var context = new SimpleLibraryContext(options);
+
+var repository = new Repository<BookModel>(context);
+var service = new DatabaseCrudServiceAsync<BookModel>(repository);
+
+Console.WriteLine("=== Лабораторна робота №5 ===");
+Console.WriteLine("ASP.NET Core Identity + JWT + Roles"); ;
---
+++ бібліотекаЛАБ5/SimpleLibrary.Infrastructure/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SimpleLibrary.Infrastructure.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public string RoleName { get; set; } = "User";
+}
